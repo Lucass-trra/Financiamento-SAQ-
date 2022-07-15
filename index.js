cee62bae91c6df8832e8fd680247da01165bb892
Lucas Terra <lucasterracontato@gmail.com>
2022-07-15 14:18:26 -0300

    arquivos do projeto, ja em andamento

---
+++ index.js
@@ -0,0 +1,10 @@
+const comCarenciaInput = document.querySelector("#comCarencia");
+const comCarenciaSelect = document.querySelector("#opcoesCarencia");
+
+comCarenciaInput.addEventListener("change",function(){
+    if(this.checked) {
+        comCarenciaSelect.removeAttribute("hidden");
+    }else {
+        comCarenciaSelect.setAttribute("hidden","hidden");
+    }
+});
