5024f9b18bce5d11354be20f9eb084fc98184fb0
Lucas Terra <lucasterracontato@gmail.com>
2022-07-17 20:02:30 -0300

    adicionando atributo -module- e arquivo -financiamneto.js-

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const comCarenciaSelect = document.querySelector("#opcoesCarencia");
 comCarenciaInput.addEventListener("change",function(){
     if(this.checked) {
         comCarenciaSelect.removeAttribute("hidden");
+
     }else {
         comCarenciaSelect.setAttribute("hidden","hidden");
     }
