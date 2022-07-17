bc758e53e2f7e33f363d8758a5e2eaa27d9adf75
Lucas Terra <lucasterracontato@gmail.com>
2022-07-17 20:29:08 -0300

    criando classe parcela,criando atributos iniciais

--- financiamento.js
+++ financiamento.js
@@ -1,6 +1,14 @@
-class financiamento {
-    
-    constructor() {
+import { parcelas } from "./parcelas";
 
-    }
+class financiamento {
+#taxaJuros; //juros mensais
+#prazo;   //em meses 
+#parcelas = [];
+
+    constructor(valor,entrada,taxaJuros,prazo) {
+        this.#taxaJuros = taxaJuros;
+        this.#prazo = prazo;
+        this.#parcelas.push(parcelas());
+
+    };
 }
---
+++ parcelas.js
@@ -0,0 +1,15 @@
+ export class parcelas {
+ #numero;
+ #valor;
+ #juros;
+ #amortizacao;
+ #saldo;
+    constructor(numero,valor,juros,amortizacao,saldo) {
+        this.#numero = numero;
+        this.#valor = valor;
+        this.#juros = juros;
+        this.#amortizacao = amortizacao
+        this.#saldo = saldo;
+
+    }
+}
