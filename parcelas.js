29f9f4d3ca33fbf322b4506267652ccdbd948681
Lucas Terra <lucasterracontato@gmail.com>
2022-07-18 21:40:09 -0300

    criando método para retornar atributos privados da classe parcelas

--- parcelas.js
+++ parcelas.js
@@ -11,8 +11,18 @@
         this.#amortizacao = amortizacao
         this.#saldo = saldo;
     }
-
     getSaldo() {
         return this.#saldo
     };
+
+    getDadosFormatados() {
+        const dados = [];
+        dados.push(this.#numero);
+        dados.push(this.#valor.toLocaleString("pt-BR",{style: "currency",currency: "BRL"}));
+        dados.push(this.#amortizacao.toLocaleString("pt-BR",{style: "currency",currency: "BRL"}));
+        dados.push(this.#juros.toLocaleString("pt-BR",{style: "currency",currency: "BRL"}));
+        dados.push(this.#saldo.toLocaleString("pt-BR",{style: "currency",currency: "BRL"}));
+        
+        return dados;
+    }
 }
